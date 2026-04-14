5c07125b17373cb304bdf072254c4062b37d60be
Yamile Gutierrez <escuderoyg28@gmail.com>
2026-04-13 22:03:59 -0500

    Clean up WSGI config by removing unnecessary comments and reordering imports

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,13 +1,10 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
 from django.core.wsgi import get_wsgi_application
 
 env = os.getenv('DJANGO_ENV', 'local')
